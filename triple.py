40af37c191ed581ed68e54b69c9f320693f3dab0
Eugene Chan <eugene.chan@sjsu.edu>
2021-10-13 14:46:51 -0700

    edit for triple.py

--- triple.py
+++ triple.py
@@ -1,1 +1,11 @@
-tripler
+#defining a decorator
+def tripler(function):
+    def wrap_1():
+    	def wrap_2():
+	    def wrap_3():
+        	 function()
+    return wrap_1
+
+def my_function():
+    print("Hello World")
+    
