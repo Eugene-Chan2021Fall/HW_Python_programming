7bf29fa0ab844168196bde75473f18531017ddf4
Eugene Chan <eugene.chan@sjsu.edu>
2021-10-12 22:25:13 -0700

    edit on constructor for timing.py

--- timing.py
+++ timing.py
@@ -1,3 +1,16 @@
 import time
 
-calculate time
+def calculate_time(func):
+    def inner(*arg):
+        seconds=time.time()
+        res = func(*arg)
+        print("Total time "+str(time.time()-t))
+        return res
+    return inner
+
+@calculate_time
+def 2_Funct(n):
+    time.sleep(n)
+
+if __name__ == "__main__":
+    2_Funct(2)
