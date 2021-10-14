b3ab0ced5561e2617a00061eb46b98675ef9d110
Eugene Chan <eugene.chan@sjsu.edu>
2021-10-13 21:18:15 -0700

    fix error

--- multiply.py
+++ multiply.py
@@ -3,7 +3,6 @@ def multiply_list():
     num_list=strg_input.split()
     for i in range(len(num_list)):
         num_list[i]=int(num_list[i])
-    total=0
-    total = [x*(x+1) for x in num_list]
+   # total = [x*(x+1) for x in num_list]
     
     return total    
