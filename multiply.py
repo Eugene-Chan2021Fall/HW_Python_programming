686aa250ea3d92bb937cc1a1f0aa0bc49de9592d
Eugene Chan <eugene.chan@sjsu.edu>
2021-10-13 21:10:01 -0700

    fix error multiply.py

--- multiply.py
+++ multiply.py
@@ -1,6 +1,5 @@
 def multiply_list():
     strg_input=input("Enter a list of integers with space between")
-    print("\n")
     num_list=strg_input.split()
     for i in range(len(num_list)):
         num_list[i]=int(num_list[i])
