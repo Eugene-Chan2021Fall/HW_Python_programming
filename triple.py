62391fcaf7bfcc52fdfa2cb73bdf89d5dfe6ef46
Eugene Chan <eugene.chan@sjsu.edu>
2021-10-14 23:48:23 -0700

    small edits

--- triple.py
+++ triple.py
@@ -4,11 +4,11 @@ defining a decorator, calling the function three times
 '''
 def tripler(my_function):
     def wrap_1():
-    	def wrap_2():
-	    def wrap_3():
-        	 my_function()
+		def wrap_2():
+		def wrap_3():
+			 my_function()
 		 return wrap_3
-	    return wrap_2
+		return wrap_2
     return wrap_1
 
 def my_function():
